496c357b195d74394ccc051972078d96ef9ca9da
gunawanherudev <60646415+gunawanherudev@users.noreply.github.com>
2021-01-08 14:37:34 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg install python2 -y
+pkg install python-pip
+pkg install curl -y
+pkg install termux-api -y
+pip2 install mime
+pip2 install colorama
+pip2 install requests
+pip2 install bs4
+pip2 install rsa
+pip2 install pyaes
+pip2 install async_generator
+pip2 install telethon
