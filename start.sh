e83c968b3186e6664431714ad7d574bda7d0fbf6
gunawanherudev <60646415+gunawanherudev@users.noreply.github.com>
2022-09-01 07:31:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,15 +11,15 @@ ulang="y"
 while [ $ulang = "y" ]
 do
 
-python yawakil.py &
-python yawakil.py
+python yk.py &
+python yk.py
 
 x=30
 while [ $x -gt 0 ]
 do
 sleep 1s
 clear
-echo " \033[1;32m Mulai ulang sisa Waktu anda $x detik. Yaa WakiiiL Yaa WakiiiL yg banyak!!"
+echo " \033[1;32m Mulai ulang sisa Waktu anda $x detik. !!"
 x=$(( $x - 1 ))
 done
 
